b638f42c850154e0bc5c572e17672ec711333725
Francisco Arguedas <farguedas99@gmail.com>
2023-01-26 19:11:53 -0600

    fix(frontend): Fixed issue with List not being initialized

--- usersapp/lib/main.dart
+++ usersapp/lib/main.dart
@@ -16,7 +16,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Map data;
-  late List usersData;
+  late List usersData = [];
 
   getUsers() async {
     http.Response response =
